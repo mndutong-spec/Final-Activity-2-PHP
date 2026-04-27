77f30472ca1157d336555a4b7778375632c8c218
mndutong-spec <mndutong@paterostechnologicalcollege.edu.ph>
2026-04-27 22:53:21 +0800

    Update Mini Project.php

--- Mini Project.php
+++ Mini Project.php
@@ -1,1 +1,14 @@
+# -Final-Activity-2-
 
+<?php
+
+echo "MINI-PROJECT: Calculator";
+$num1 = 11;
+$num2 = 13;
+
+echo "Sum: " . ($num1 + $num2) . "<br>";
+echo "Difference: " . ($num1 - $num2) . "<br>";
+echo "Product: " . ($num1 * $num2) . "<br>";
+echo "Quotient: " . ($num1 / $num2) . "<br>";
+
+?>
