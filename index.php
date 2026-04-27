8b4959985474520c8bf088d6802f3c331a23e6ec
mndutong-spec <mndutong@paterostechnologicalcollege.edu.ph>
2026-04-27 22:12:51 +0800

    Add guide questions and answers to index.php

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 echo "Activity 1 <br>";
 echo "Name: Marieden Narvaez Dutong <br>";
 print "Course: BSIT";
+
 ?>
+
+Guide Questions:
+1. What symbol is used to end a PHP statement?
+answer:    ➤ The semicolon ( ; ) is used to end a PHP statement.
+2. Which is faster: echo or print?
+answer:    ➤ echo is faster than print because it doesn’t return a value, while print does.
